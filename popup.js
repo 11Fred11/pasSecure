3940256db45da1a8182c6650f0a740112806229e
10Fred10 <contact.hammami.fredj@gmail.com>
2019-05-12 23:33:54 +0100

    BackEnd done

--- popup.js
+++ popup.js
@@ -10,6 +10,7 @@ window.onload = function() {
   let password = document.getElementById("password");
   let meter = document.getElementById("password-strength-meter");
   let textBox = document.getElementById("password-strength-text");
+  let passBox = document.getElementById("password-found-text");
 
   function hashPass(password) {
     let hash = sHa1(password);
@@ -21,58 +22,62 @@ window.onload = function() {
     if (password.value == "") {
       meter.hidden = true;
       textBox.innerHTML = "";
+      passBox.innerHTML = "";
     } else {
       meter.hidden = false;
+      let { firstFive, reminder } = hashPass(password.value);
       let result = zxcvbn(password.value);
-      console.log(result.score);
-      if (result.score == 0) score = 1;
-      else score = result.score;
-      meter.value = score;
-      textBox.innerHTML =
-        "Strength: " +
-        "<strong>" +
-        strength[result.score] +
-        "</strong>" +
-        "<span class='feedback'>" +
-        result.feedback.warning +
-        " " +
-        result.feedback.suggestions +
-        "</span";
-    }
-
-    //let { firstFive, reminder } = hashPass(password.value);
-    //console.log(firstFive, reminder);
-
-    /* fetch(`https://api.pwnedpasswords.com/range/${firstFive}`)
-      .then(response => response.text())
-      .then(text => {
-        let newArray = [];
-        text.split("\n").forEach(row => {
-          let objRow = {};
-          splittedRow = row.split(":");
-          objRow.hash = splittedRow[0];
-          objRow.occ = splittedRow[1];
-          newArray.push(objRow);
-        });
-        let found = false;
-        let occ = 0;
-        for (elem of newArray) {
-          if (elem.hash == reminder) {
-            found = true;
-            occ = elem.occ;
-            break;
+      console.log(result);
+      fetch(`https://api.pwnedpasswords.com/range/${firstFive}`)
+        .then(response => response.text())
+        .then(text => {
+          let newArray = [];
+          text.split("\n").forEach(row => {
+            let objRow = {};
+            splittedRow = row.split(":");
+            objRow.hash = splittedRow[0];
+            objRow.occ = splittedRow[1];
+            newArray.push(objRow);
+          });
+          let found = false;
+          let occ = 0;
+          for (elem of newArray) {
+            if (elem.hash == reminder) {
+              found = true;
+              occ = elem.occ;
+              break;
+            }
           }
-        }
-        if (found) {
-          text1.innerHTML = `Found ! : ${occ} times`;
-          meter.hidden = false;
-          meter.value = "1";
-        } else {
-          text1.innerHTML = "Password Safe :) ";
-          meter.hidden = false;
-          meter.value = "4";
-        }
-      })
-      .catch(err => console.log(err)); */
+          if (found) {
+            passBox.innerHTML = `This password matches : ${occ} HACKED accounts`;
+            meter.hidden = false;
+            meter.value = "5";
+          } else {
+            if (result.score == 0) score = 1;
+            else score = result.score;
+            passBox.innerHTML = "This Password didn't match any hacked account";
+            meter.hidden = false;
+            meter.value = score;
+          }
+          let newSuggestion = "";
+          if (result.score == 4) {
+            newSuggestion =
+              " Congratulations! Your password seems very strong. Now careful where to save it.";
+          } else
+            newSuggestion =
+              "<span class='feedback'>" +
+              result.feedback.warning +
+              " " +
+              result.feedback.suggestions +
+              "</span";
+          textBox.innerHTML =
+            "Strength: " +
+            "<strong>" +
+            strength[result.score] +
+            "</strong> " +
+            newSuggestion;
+        })
+        .catch(err => console.log(err));
+    }
   });
 };
