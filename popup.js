a0c6f4e1bb346d2bfb5f984b0dce24294c6d7977
10Fred10 <contact.hammami.fredj@gmail.com>
2019-05-12 22:52:50 +0100

    implemented zxcvbn

--- popup.js
+++ popup.js
@@ -1,17 +1,49 @@
 window.onload = function() {
+  var strength = {
+    0: "Terrible",
+    1: "Bad",
+    2: "Meeh.",
+    3: "Good",
+    4: "Strong"
+  };
   let go = document.getElementById("go");
+  let password = document.getElementById("password");
+  let meter = document.getElementById("password-strength-meter");
+  let textBox = document.getElementById("password-strength-text");
+
   function hashPass(password) {
     let hash = sHa1(password);
     let firstFive = hash.substr(0, 5).toUpperCase();
     let reminder = hash.substr(5).toUpperCase();
     return { firstFive, reminder };
   }
-  go.onclick = function(element) {
-    let { firstFive, reminder } = hashPass(
-      document.getElementById("pass").value
-    );
-    console.log(firstFive, reminder);
-    fetch(`https://api.pwnedpasswords.com/range/${firstFive}`)
+  password.addEventListener("input", function(element) {
+    if (password.value == "") {
+      meter.hidden = true;
+      textBox.innerHTML = "";
+    } else {
+      meter.hidden = false;
+      let result = zxcvbn(password.value);
+      console.log(result.score);
+      if (result.score == 0) score = 1;
+      else score = result.score;
+      meter.value = score;
+      textBox.innerHTML =
+        "Strength: " +
+        "<strong>" +
+        strength[result.score] +
+        "</strong>" +
+        "<span class='feedback'>" +
+        result.feedback.warning +
+        " " +
+        result.feedback.suggestions +
+        "</span";
+    }
+
+    //let { firstFive, reminder } = hashPass(password.value);
+    //console.log(firstFive, reminder);
+
+    /* fetch(`https://api.pwnedpasswords.com/range/${firstFive}`)
       .then(response => response.text())
       .then(text => {
         let newArray = [];
@@ -31,10 +63,16 @@ window.onload = function() {
             break;
           }
         }
-        if (found)
-          document.getElementById("parag").innerHTML = `Found ! : ${occ} times`;
-        else document.getElementById("parag").innerHTML = "Password Safe :) ";
+        if (found) {
+          text1.innerHTML = `Found ! : ${occ} times`;
+          meter.hidden = false;
+          meter.value = "1";
+        } else {
+          text1.innerHTML = "Password Safe :) ";
+          meter.hidden = false;
+          meter.value = "4";
+        }
       })
-      .catch(err => console.log(err));
-  };
+      .catch(err => console.log(err)); */
+  });
 };
